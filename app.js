81afc21cd9bc871665bceeb1707e83e1979d868e
Sayem995 <sayem.bokhari.1@gmail.com>
2020-02-05 19:29:50 +0000

    created new link element and appended the link to li

--- app.js
+++ app.js
@@ -24,7 +24,13 @@ function addTask(e) {
     
     li.className = 'collection-item';
 
+    li.appendChild(document.createTextNode(taskInput.value));
 
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+    
 
 
 
